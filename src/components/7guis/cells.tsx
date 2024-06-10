fd298e297a72644581ce233fb5e57591b574ace1
mancuoj <mancuoj@gmail.com>
2024-06-10 14:17:51 +0800

    feat: table cells

--- src/components/7guis/cells.tsx
+++ src/components/7guis/cells.tsx
@@ -1,9 +1,27 @@
 import { ScrollArea, ScrollBar } from '../ui/scroll-area'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 
 export function Cells() {
   return (
-    <ScrollArea className="size-96 rounded-md border">
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+    <Table>
+      <ScrollArea className="size-96 rounded-md border pb-3 pr-3">
+        <TableHeader>
+          <TableRow>
+            <TableHead></TableHead>
+            {Array.from({ length: 26 }).map((_, i) => (
+              <TableHead key={i}>{String.fromCharCode(65 + i)}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: 100 }).map((_, i) => (
+            <TableRow key={i}>
+              <TableCell>{i}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </Table>
   )
 }
